a9165cd11ccdc29473e8c09be6257e8b4e3e5432
JoeKnowles-Jr <89408846+JoeKnowles-Jr@users.noreply.github.com>
2021-09-22 11:08:13 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// hold app secrets and config
-
-module.exports = {
-    "connectionString": "mongodb+srv://mirroradmin:nB886vZYq4xb3kzy@cluster0.2rk02.mongodb.net/channel?retryWrites=true&w=majority",
-    "secret": "airotciv"
-};
